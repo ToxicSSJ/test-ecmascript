f12b6babc1aec0d278ebc64a4dffd56f2cfa98b8
zlToxicNetherlz <abrahamlora3@gmail.com>
2020-04-17 00:16:12 -0500

    funciones asincronas (async await)

--- src/es8/index.js
+++ src/es8/index.js
@@ -31,3 +31,35 @@ console.log(string.padEnd(12, ' -----')) // permite hacer formatos de tablas
 const obj = {
     name: 'abraham', // no lanza error (mas amigable es8)
 }
+
+// continuación @_@
+// 17/04/2020
+// creamos una promesa
+const helloWorld = () => {
+    return new Promise((resolve, reject) => { // instancia la promesa
+        (true)
+         ? setTimeout(() => resolve('Hello World'), 3000) // espera 3 segundos
+         : reject(new Error('Test Error')) // lanza error si salió mal
+    })
+}
+
+const helloAsync = async () => {
+    const hello = await helloWorld() // espera a que suceda el resolve
+    console.log(hello) // imprimo el resultado
+}
+
+helloAsync(); // llamo la función
+
+// nueva función con
+// try y catch para
+// poder manejar errores
+const anotherFunction = async () => {
+    try {
+        const hello = await helloWorld() // espero a que la función se complete
+        console.log(hello) // imprimo Hello World
+    } catch(error) {
+        console.log(error) // imprimo el error con stacktrace
+    }
+}
+
+anotherFunction(); // se llama a la nueva función
